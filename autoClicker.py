8671202a04e8ef783fd8b1b7179c1fcf8f1aafdd
Rohan Kumar <71839773+rohan2007@users.noreply.github.com>
2020-12-30 21:41:28 +0530

    Update autoClicker.py

--- autoClicker.py
+++ autoClicker.py
@@ -2,13 +2,12 @@ import pyautogui
 import time
 
 # !!!!! ATTENTION !!!!!
-
-# you have to edit pyautogui.moveTo(50,160) file according to your screen
+# you have to edit pyautogui.moveTo(50,110) file according to your screen
 
 time.sleep(10)
 
-pyautogui.moveTo(50,160)
+pyautogui.moveTo(50, 510)
 
 while True:
-    time.sleep(1)
-    pyautogui.click()
+     time.sleep(1)
+     pyautogui.click()
